970df470e95b3f2a953cdf2e4745e4856d7e1c61
Angel Slavchev <angel.slavchev@mossgoblin.com>
2020-11-17 22:30:49 +0200

    Added biome creation

--- Assets/BiomeTrash/GridCreator.cs
+++ Assets/BiomeTrash/GridCreator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using ToolBox;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -14,6 +15,8 @@ public class GridCreator : MonoBehaviour
     public int seed;
     public float rndMin;
     public float rndMax;
+    private float threshold;
+    public float decrement;
     public int numberOfBiomes;
     public Color[] colors;
 
@@ -25,10 +28,23 @@ public class GridCreator : MonoBehaviour
     private Transform[,] gridObj;
     private int[,] gridBiomes;
 
+    // Flooding
+    List<Vector3> objList;
+    // Tiles to be checked
+    Dictionary<int, Stack<Vector3>> tilesToCheck;
+
+    // Claimed tiles
+    int[,] tilesChecked;
+
     public void Start()
     {
         CreateGrid();
         CreateBiomes();
+    }
+
+    public void Update()
+    {
+        // StartCoroutine("FloodGrid");
     }
 
     private void CreateGrid()
@@ -78,6 +94,7 @@ public class GridCreator : MonoBehaviour
 
     private void CreateCenters()
     {
+        objList = new List<Vector3>();
         int intervals = numberOfBiomes + 1;
         int intervalsLength = ((int)width / (numberOfBiomes * 2));
         int lowerLine = (int)(depth / 6) - 1;
@@ -90,18 +107,130 @@ public class GridCreator : MonoBehaviour
             int posW = (count * 2 + 1) * intervalsLength + count;
             int posD = (count % 2 == 0) ? upperLine : lowerLine;
             gridObj[posW, posD].GetChild(0).GetComponent<MeshRenderer>().material.color = colors[count];
+            objList.Add(new Vector3(posW, 0, posD));
             Debug.Log($"{count}: {posW}/{posD}");
         }
     }
 
-
     private void FloodGrid()
     {
-        Dictionary<int, List<Vector3>> biomesCheckedTiles;
+
+        threshold = (rndMin + rndMax) / 2;
+
+        bool checking = true;
+
+        tilesToCheck = new Dictionary<int, Stack<Vector3>>();
+        tilesChecked = new int[width, depth];
+        // put the biome seeds in the queue
+        for (int count = 0; count < numberOfBiomes; count++)
+        {
+            tilesToCheck.Add(count, new Stack<Vector3>());
+            tilesToCheck[count].Push(objList[count]);
+        }
+
+        int dbgCount = 0;
+
+        while (checking)
+        {
+            int markedTiles = 0;
+
+            // iterate all biomes
+            for (int count = 0; count < numberOfBiomes; count++)
+            {
+                // pull next tile
+                if (tilesToCheck[count].Count == 0)
+                {
+                    Debug.Log($"Dict {count} empty; checked {markedTiles}");
+                    continue;
+                }
+                Vector3 currentTile = tilesToCheck[count].Pop();
+
+                // enqueue unckecked nbrs
+                List<Vector3> nbrs = GetNbrs(currentTile);
+                foreach (Vector3 nbr in nbrs)
+                {
+                    tilesToCheck[count].Push(nbr);
+                }
+
+                // check the current tile
+                if (gridRnd[(int)currentTile.x, (int)currentTile.z] >= threshold)
+                {
+                    // If the tile is unsuitable - adjust and add back to the queue
+                    gridRnd[(int)currentTile.x, (int)currentTile.z] = Mathf.Min(gridRnd[(int)currentTile.x, (int)currentTile.z], gridRnd[(int)currentTile.x, (int)currentTile.z]- decrement);
+                    tilesToCheck[count].Push(currentTile);
+                }
+                else
+                {
+                    gridBiomes[(int)currentTile.x, (int)currentTile.z] = count + 1;
+                    // DBG InProcess coloring
+                    gridObj[(int)currentTile.x, (int)currentTile.z].GetChild(0).GetComponent<MeshRenderer>().material.color = colors[count];
+                    markedTiles++;
+                }
+            }
+
+            int empty = 0;
+            for (int count = 0; count < tilesToCheck.Count; count ++)
+            {
+                if (tilesToCheck[0].Count <= 0)
+                {
+                    empty++;
+                    checking = false;
+                }
+            }
+
+            if (dbgCount > (width * depth * 10))
+            {
+                Debug.Log($"Cycled out at {dbgCount}");
+                break;
+            }
+
+            dbgCount++;
+
+        }
     }
 
-    private bool FreeTile()
+    private List<Vector3> GetNbrs(Vector3 tile)
     {
+        List<Vector3> result = new List<Vector3>();
+        for (int countW = -1; countW < 2; countW++)
+        {
+            for (int countD = -1; countD < 2; countD++)
+            {
+                if (Mathf.Abs(countW) + Mathf.Abs(countD) == 1)
+                {
+                    if (FreeTile(countW + (int)tile.x, countD + (int)tile.z))
+                    {
+                        result.Add(new Vector3(countW + (int)tile.x, 0, countD + (int)tile.z));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+    private bool FreeTile(int posW, int posD)
+    {
+        if ((posW < 0) ||
+            (posW >= width) ||
+            (posD < 0) ||
+            (posD >= depth))
+        {
+            return false;
+        }
+
+        for (int count = 0; count < numberOfBiomes; count++)
+        {
+            if (tilesToCheck[count].Contains(new Vector3(posW, 0, posD)))
+            {
+                return false;
+            }
+        }
+
+        if (gridBiomes[posW, posD] > 0)
+        {
+            return false;
+        }
+
         return true;
     }
 }
